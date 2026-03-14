ac899f7a38c38e807790548e59261e96a31713f7
Gabriel Correa <gabrielcrr1710@gmail.com>
2026-03-14 17:45:49 -0300

    feat: Implement the initial chess board UI with basic drag-and-drop piece movement.

--- app/components/ChessBoard/BoardBits/Files.tsx
+++ app/components/ChessBoard/BoardBits/Files.tsx
@@ -2,7 +2,7 @@ export default function Files() {
     const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     return (
         <div className="col-2 flex justify-around">
-            {files.map(file => <div className="text-[var(--light-square-color)]">{file}</div>)}
+            {files.map(file => <div key={file} className="text-[var(--light-square-color)]">{file}</div>)}
         </div>
     )
 }
--- app/components/ChessBoard/BoardBits/Ranks.tsx
+++ app/components/ChessBoard/BoardBits/Ranks.tsx
@@ -2,7 +2,7 @@ export default function Ranks() {
     const ranks = [8,7,6,5,4,3,2,1]
     return (
         <div className="flex flex-col justify-around">
-            {ranks.map(rank => <div className="text-[var(--light-square-color)]">{rank}</div>)}
+            {ranks.map(rank => <div key={rank} className="text-[var(--light-square-color)]">{rank}</div>)}
         </div>
     )
 }
